5b5d69da9b778d2f9c67ae7f44dc703779ffc3cc
alavieille <amaury.lavieille@businessdecision.com>
2019-10-06 20:57:00 +0200

    add js doc

--- src/AbstractComponent.js
+++ src/AbstractComponent.js
@@ -1,10 +1,17 @@
 /* eslint class-methods-use-this: "off", no-unused-vars: "off" */
 
 export default class AbstractComponent {
+
+  /**
+   * @param {Application} application
+   */
   constructor(application) {
     this.application = application;
   }
 
+  /**
+   * @param {Element} el
+   */
   render(el) {
     throw new Error('Method start should be implement');
   }
